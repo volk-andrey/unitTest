2e874f95fd3ad5adf32b1537cd2ac8b34785c7bc
Andrey <andrey.volk.1999@gmail.com>
2024-09-29 22:14:12 +1000

    Initial commit

---
+++ src/test/java/sky/pro/unitTest/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.unitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
